579f5df005bdf6c87cb561cfb9da77ba4f347e45
S Rabi <rabi.sanaye@yahoo.com>
2022-08-23 13:13:58 +0200

    m

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1:
+console.log([4, 2, 5, 3, 9, 7, 1, 8, 2, 6].sort((a, b) => a - b));
+
+console.log("------------------------------------------");
+
+// 2:
+let arraySort = ["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"];
+console.log(arraySort.sort());
+
+console.log(["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"].sort());
+
+console.log("------------------------------------------");
